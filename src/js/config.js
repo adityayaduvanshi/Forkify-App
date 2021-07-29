17027af01895eb1331e3ee910ba91fcd00142fe4
aditya <aditya97y@gmail.com>
2021-07-29 12:26:05 +0530

    Initial Coomit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'c5c34523-3231-46fb-99c2-841790b1b283';
+export const MODAL_CLOSE_SEC = 2.5;
